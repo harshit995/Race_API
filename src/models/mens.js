d82570162a072703a94a1d84a7e766f261809c01
Harshit <gharshit89@gmail.com>
2023-12-09 13:30:12 +0530

    update

--- src/models/mens.js
+++ src/models/mens.js
@@ -1,1 +1,35 @@
-const express=require('express');
+const mongoose=require('mongoose');
+
+const menSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    ranking:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    dob:{
+        type:Date,
+        required:true,
+        trim:true
+    },
+    country:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    score:{
+        type:Number,
+        required:true,
+        trim:true
+    },
+    event:{
+        typr:String,
+        default:"100m"
+    }
+})
+
+const MensRanking = new mongoose.model()
